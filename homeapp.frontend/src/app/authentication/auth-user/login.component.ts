c4235ab7ac24e7524e1478de0cc9134953f078f1
Andrej Miller <andrej_miller@outlook.de>
2024-12-14 15:44:46 +0100

    fix login

--- homeapp.frontend/src/app/authentication/auth-user/login.component.ts
+++ homeapp.frontend/src/app/authentication/auth-user/login.component.ts
@@ -64,7 +64,7 @@ export class LoginComponent implements OnInit {
       clientURI: `${environment.baseUrl}/authentication/forgotpassword`,
     };
 
-    this.authService.loginUser('login', userForAuth).subscribe({
+    this.authService.loginUser('authentication/login', userForAuth).subscribe({
       next: (res: AuthResponseDto) => {
         if (res.is2StepVerificationRequired) {
           this.router.navigate(['/authentication/twostepverification'], {
