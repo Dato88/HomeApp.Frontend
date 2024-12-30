c462f9ea51ea5e66d99cb688787bcedb94be86a1
Andrej Miller <andrej_miller@outlook.de>
2024-12-30 14:12:47 +0100

    quick delete button design

--- homeapp.frontend/src/app/todo/todo.component.ts
+++ homeapp.frontend/src/app/todo/todo.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TodoDto } from '../shared/_interfaces/todo/todo-dto';
 import { MatIconModule } from '@angular/material/icon';
-import { AsyncPipe, DatePipe, NgStyle } from '@angular/common';
+import { AsyncPipe, DatePipe, NgClass, NgStyle } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { TodoActions } from '../shared/+store/todo/todo.actions';
 import { Observable } from 'rxjs';
@@ -34,6 +34,8 @@ export class TodoComponent {
   todos$: Observable<TodoDto[]>;
   loading$: Observable<boolean>;
 
+  private touchStartX: number = 0;
+
   constructor(private store: Store) {
     this.errorMessage = '';
     this.showError = false;
@@ -59,4 +61,27 @@ export class TodoComponent {
   deleteTodo(id: number): void {
     this.store.dispatch(TodoActions.deleteTodo({ id }));
   }
+
+  onTouchStart(event: TouchEvent, todoId: number): void {
+    this.touchStartX = event.touches[0].clientX;
+  }
+
+  onTouchMove(event: TouchEvent, todoId: number): void {
+    const touchEndX = event.touches[0].clientX;
+    const todoElement = document
+      .getElementById(`todo-item-${todoId}`)
+      ?.getElementsByClassName('delete-button')[0];
+
+    if (this.touchStartX - touchEndX > 10) {
+      if (todoElement && !todoElement.classList.contains('swiped')) {
+        todoElement.classList.toggle('swiped');
+      }
+    } else if (touchEndX - this.touchStartX > 10) {
+      if (todoElement && todoElement.classList.contains('swiped')) {
+        todoElement.classList.toggle('swiped');
+      }
+    }
+  }
+
+  onTouchEnd(event: TouchEvent, todoId: number): void {}
 }
